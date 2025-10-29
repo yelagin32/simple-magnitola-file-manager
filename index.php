9a9bdc777997268c72be13dae583c51c238e47cb
yelagin32 <127603795+yelagin32@users.noreply.github.com>
2025-10-29 12:00:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -80,6 +80,11 @@ if (isset($_SESSION['authenticated'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 </head>
 <body class="bg-light">
     <div class="container py-5">
